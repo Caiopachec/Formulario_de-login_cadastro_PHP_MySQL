6c12fe97708e6180020a977117ba54ba139f00ad
Caiopachec <caiopacheco936@gmail.com>
2025-11-27 20:34:22 -0300

    segundo comit

--- cadastro.php
+++ cadastro.php
@@ -8,7 +8,18 @@
 <body>
     <div class="container">
         <form action="cadastro" method="post">
-            
+            <div class="grupo_formularios">
+                <input type="text" name="nome_completo" placeholder="Nome completo:">
+            </div>
+            <div class="grupo_formularios">
+                <input type="emailc" name="email" placeholder="Email:">
+            </div>
+            <div class="grupo_formularios">
+                <input type="senha" name="senha" placeholder="Senha:">
+            </div>
+            <div class="grupo_formularios">
+                <input type="text" name="repita_senha" placeholder="Digite a senha novamente:">
+            </div>
         </form>
     </div>
 </body>
